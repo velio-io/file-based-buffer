b3499c9f1b5eaaac9ff3cd8312f54cb9259550a8
Waldemar <waldemar.schwan@uswitch.com>
2016-05-12 09:54:41 +0100

    use provided libs this is to reduce depencency conflicts also bumped the version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject file-based-buffer "0.1.2"
+(defproject file-based-buffer "0.1.3"
   :description "A file backed core.async buffer for channels."
   :url "https://github.com/Velrok/file-based-buffer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/core.async "0.2.374"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/tools.logging "0.3.1" :scope "provided"]
+                 [org.clojure/core.async "0.2.374" :scope "provided"]
                  [com.squareup/tape "1.2.3"]
-                 [com.taoensso/nippy "2.11.1"]])
+                 [com.taoensso/nippy "2.11.1" :scope "provided"]])
